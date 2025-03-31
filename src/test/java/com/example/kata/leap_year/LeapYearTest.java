83c0a08af4d3c8416ff53219fe32ce065f045a38
hyemin <hyemin@ktown4u.com>
2025-03-31 22:09:05 +0900

    add assertion

--- src/test/java/com/example/kata/leap_year/LeapYearTest.java
+++ src/test/java/com/example/kata/leap_year/LeapYearTest.java
@@ -1,10 +1,9 @@
 package com.example.kata.leap_year;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 /**
  * 0, 음수, null을 입력하면 예외를 던진다.
@@ -16,14 +15,17 @@ import static org.assertj.core.api.Assertions.*;
 public class LeapYearTest {
 
     @Test
-    @DisplayName("0, 음수, null을 입력하면 예외를 던진다.")
+    @DisplayName("0, 음수를 입력하면 예외를 던진다.")
     public void case0() {
         assertThatThrownBy(() -> leapYear(0))
+                .message()
+                .isEqualTo("자연수만 입력할 수 있습니다.");
+        assertThatThrownBy(() -> leapYear(-1))
                 .message()
                 .isEqualTo("자연수만 입력할 수 있습니다.");
     }
 
     private void leapYear(final int year) {
-        throw new IllegalStateException("LeapYearTest::leapYear not implemented yet");
+        throw new IllegalStateException("자연수만 입력할 수 있습니다.");
     }
 }
