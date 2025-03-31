ae6fccdb4d327d8ef3b41e140de488d2f9cb2f80
hyemin <hyemin@ktown4u.com>
2025-03-31 22:54:00 +0900

    add test case

--- src/test/java/com/example/kata/leap_year/TennisTest.java
+++ src/test/java/com/example/kata/leap_year/TennisTest.java
@@ -12,6 +12,12 @@ public class TennisTest {
         assertThat(tennis(0, 0)).isEqualTo("Love : Love");
     }
 
+    @Test
+    @DisplayName("스코어가 1:3이면 Fifteen : Thirty로 표시된다.")
+    public void case1() {
+        assertThat(tennis(1, 3)).isEqualTo("Fifteen : Thirty");
+    }
+
     private String tennis(final int score1, final int score2) {
         if (0 == score1 && 0 == score2) return "Love : Love";
         return "";
